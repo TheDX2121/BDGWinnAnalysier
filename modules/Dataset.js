4405bf85382bbbb63d782f883df8294af435d090
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-08-08 11:16:13 +0530

    Create Dataset.js

---
+++ modules/Dataset.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const datasetSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 100
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Dataset", datasetSchema);
