040a475a1817608cba476290be911951e67f5fc9
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-08-08 11:16:50 +0530

    Create Event.js

---
+++ modules/Event.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    datasetId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Dataset",
+      required: true,
+      index: true
+    },
+
+    first: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 9
+    },
+
+    second: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 9
+    },
+
+    next: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 9
+    },
+
+    resultType: {
+      type: String,
+      enum: ["Big", "Small"],
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+eventSchema.index({
+  datasetId: 1,
+  first: 1,
+  second: 1
+});
+
+module.exports = mongoose.model("Event", eventSchema);
