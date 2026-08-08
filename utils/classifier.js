c8c2e354bae87dc56857bdcb1ddcf9b62a5310b6
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-08-08 11:21:59 +0530

    Create utils/classifier.js

---
+++ utils/classifier.js
@@ -0,0 +1,20 @@
+function getSize(number) {
+  if (
+    typeof number !== "number" ||
+    !Number.isInteger(number) ||
+    number < 0 ||
+    number > 9
+  ) {
+    throw new Error(
+      "Outcome must be an integer from 0 to 9."
+    );
+  }
+
+  return number >= 5
+    ? "Big"
+    : "Small";
+}
+
+module.exports = {
+  getSize
+};
