11e472dc53ef9c8d6e75b895d68b4f28bcea7bff
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-08-08 11:30:09 +0530

    Update pattern.js

--- utils/pattern.js
+++ utils/pattern.js
@@ -1,17 +1,4 @@
 function createPattern(first, second) {
-  if (
-    !Number.isInteger(first) ||
-    !Number.isInteger(second) ||
-    first < 0 ||
-    first > 9 ||
-    second < 0 ||
-    second > 9
-  ) {
-    throw new Error(
-      "Pattern values must be digits from 0 to 9."
-    );
-  }
-
   return `${first}-${second}`;
 }
 
